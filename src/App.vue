9e9655ab9a3961fe06892e51a7d3fb113bdfdc69
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2019-11-03 21:07:02 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
   <div id="app">
+    <p>Post: {{posts[0]}}</p>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        posts: [
+          'Hell0'
+        ],
+        endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+      }
+    },
+    created() {
+      this.getAllPosts();
+    },
+
+    methods: {
+      getAllPosts() {
+        axios.get(this.endpoint)
+          .then(response => {
+            this.posts = response.data;
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.log('-----error-------');
+            console.log(error);
+          })
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  padding: 30px;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
